2eee2932f1a8ee0b3eeb56abda2e193a321d645d
t_wak <wakhome@t-waknoMacBook-Pro.local>
2021-04-12 06:59:29 +0900

    Revert "sidebarが表示されない"
    This reverts commit e20df00b8902113fffbef11566e92692f6d2d345.

--- app/javascript/packs/menu_sidebar.js
+++ app/javascript/packs/menu_sidebar.js
@@ -1,16 +1,7 @@
 
 jQuery(document).on("turbolinks:load", function(){
     $("#view_sidebar").on("click", (function(){
-        var $sidebar = $("#sidebar");
-        var p1_element = document.getElementById("sidebar");
-        if($sidebar.hasClass("opened")){
-            $sidebar.css("display", "none");
-            $sidebar.removeClass("opened");
-        }else{
-            $sidebar.css("display", "block");
-            $sidebar.addClass("opened");
-        }
-        console.log(p1_element.classList.contains("opened"));
+        $("#sidebar").css("display", "block");
     }))
 })
 
